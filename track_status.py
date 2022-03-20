0449edfdc033abb28998b493d3d27d3903486851
Raj1411 <56500451+Raj1411@users.noreply.github.com>
2022-03-20 19:55:02 +0530

    Update track_status.py

--- track_status.py
+++ track_status.py
@@ -51,6 +51,8 @@ def main():
     st.markdown('<h1 style="text-align: center;">Shipment Status Tracker</h1>', unsafe_allow_html=True) 
     st.markdown('''---------------------------------------------------------------------------------------------------------''')
     st.markdown(menu_touchup,unsafe_allow_html=True)
+    
+    file_1=st.file_uploader("Upload Excel File", type=["json"])
 
     btn1=st.button('Track Shipment Status')
 
@@ -59,7 +61,7 @@ def main():
     
     
         googlesheeturl='https://docs.google.com/spreadsheets/d/17EW9CAOOwmefEP4toLAubQhj7q-NgsjLRAlzCeKvIIs/edit#gid=1757221736'
-        creds=ServiceAccountCredentials.from_json_keyfile_name("./keys.json",scope)
+        creds=ServiceAccountCredentials.from_json_keyfile_name(file_1.name,scope)
         client=gspread.authorize(creds)
         sheet=client.open_by_url(googlesheeturl)
         main_worksheet=sheet.worksheet('Sheet3')
